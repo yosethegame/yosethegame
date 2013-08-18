656fcc5956377375fccdf4593ffaeff6237a1ab8
Eric Mignot <eric.mignot@gmail.com>
2013-08-18 08:41:12 -0400

    Protect success request coming from outside yose

--- public/js/success.js
+++ public/js/success.js
@@ -9,41 +9,50 @@ findChallengeTitle = function(challengeFile, database) {
 };
 
 success = function(request, response, database) {
-	console.log('\nreceiving success request from host ' + request.headers.host);
+	var host = request.headers.host;
+	if (host != 'localhost:5000' && host != 'yose.herokuapp.com') {
+		console.log('\n !!! receiving success request from host ' + request.headers.host);
+		response.writeHead(401);
+		response.end();
+		return;
+	}
 	
 	if (request.method != 'POST') {
 		response.writeHead(405);
 		response.end();
-	} else {
-		var body = '';
-	    request.on('data', function (data) {
-	        body += data;
-	    });
-	    request.on('end', function () {
-			var form = qs.parse(body);
+		return;
+	} 
+	
+	var body = '';
+    request.on('data', function (data) {
+        body += data;
+    });
+
+    request.on('end', function () {
+		var form = qs.parse(body);
 			
-			if (form.login == undefined || form.challenge == undefined || form.server == undefined) {
-				response.writeHead(400);
-				response.end();
-				return;
-			}
-			
-			var player = database.find(form.login);
-			
-			if (player.portfolio == undefined) {
-				player.portfolio = [];
-			}
-			
-			player.portfolio.push( 
-				{ 
-					title: findChallengeTitle(form.challenge, database),
-					server: form.server 
-				} 
-			);
-			database.savePlayer(player);
+		if (form.login == undefined || form.challenge == undefined || form.server == undefined) {
+			response.writeHead(400);
 			response.end();
-	    });
-	}
+			return;
+		}
+			
+		var player = database.find(form.login);
+			
+		if (player.portfolio == undefined) {
+			player.portfolio = [];
+		}
+			
+		player.portfolio.push( 
+			{ 
+				title: findChallengeTitle(form.challenge, database),
+				server: form.server 
+			} 
+		);
+		database.savePlayer(player);
+		response.end();
+    });
+	
 };
 
 module.exports = success;
