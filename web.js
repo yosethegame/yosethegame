8ae0fc486fd177ab6ec2bbf58e3a5d6b84f874a0
Eric Mignot <eric.mignot@gmail.com>
2013-08-24 20:31:49 -0400

    Pablo!

--- web.js
+++ web.js
@@ -7,10 +7,14 @@ var server = new Server(new Router());
 var database = new ProductionFileDatabase();
 
 database.createPlayer({
-		login: 'ericminio',
-		avatar: 'http://www.gravatar.com/avatar/8274a8b8d2c4aa2f42c1bbe2f130a0a7.png'
-	});
+	login: 'ericminio',
+	avatar: 'http://www.gravatar.com/avatar/8274a8b8d2c4aa2f42c1bbe2f130a0a7.png'
+});
 
+database.createPlayer({
+	login: 'pablolis',
+	avatar: 'https://si0.twimg.com/profile_images/2225960371/PabloL_normal.jpg'
+});
 
 server.useRepository(database);
 
