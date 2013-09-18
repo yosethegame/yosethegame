3026cdffaa0b151526f88731d16274ac5ae72f64
Eric Mignot <eric.mignot@gmail.com>
2013-09-17 20:26:30 -0400

    new player: cletourneau, testinfected, andrebrissette

--- web.js
+++ web.js
@@ -16,6 +16,21 @@ database.createPlayer({
 	avatar: 'https://si0.twimg.com/profile_images/2225960371/PabloL_normal.jpg'
 });
 
+database.createPlayer({
+	login: 'cletourneau',
+	avatar: 'https://si0.twimg.com/profile_images/2168665726/profile_pic_bigger.jpg'
+});
+
+database.createPlayer({
+	login: 'testinfected',
+	avatar: 'https://si0.twimg.com/profile_images/268189950/vince_250x250_bigger.jpg'
+});
+
+database.createPlayer({
+	login: 'AndreBrissette',
+	avatar: 'https://si0.twimg.com/profile_images/1900139453/andre_brissette_200_bigger.jpg'
+});
+
 server.useDatabase(database);
 
 server.start();
