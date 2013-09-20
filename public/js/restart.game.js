4dd95655513e1f8cd5065476071d07fbeedeba07
Eric Mignot <eric.mignot@gmail.com>
2013-09-20 17:35:33 -0400

    A player can restart the game :)

--- public/js/restart.game.js
+++ public/js/restart.game.js
@@ -1,4 +1,14 @@
+var url 			= require('url');
+var thePlayer 		= require('./utils/player.utils');
+
 restartgame = function(request, response, database) {
+	var params = url.parse(request.url, true);	
+	var player = database.find(params.query.login);
+	if (player != undefined) {
+		player.portfolio = [];
+		player.server = undefined;
+		database.savePlayer(player);
+	}
 	response.end();
 }
 
