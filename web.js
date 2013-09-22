258767135a919236ee40b15ea15597f8580d93ef
Eric Mignot <eric.mignot@gmail.com>
2013-09-22 15:49:00 -0400

    player o_vaillancourt created

--- web.js
+++ web.js
@@ -6,40 +6,12 @@ var server = new Server(router);
 var database = new ProductionDatabase();
 server.useDatabase(database);
 
-var ericminio = {
-	login: 'ericminio',
-	avatar: 'http://www.gravatar.com/avatar/8274a8b8d2c4aa2f42c1bbe2f130a0a7.png'
+var ovaillancourt = {
+	login: 'o_vaillancourt',
+	avatar: 'https://si0.twimg.com/profile_images/1846958904/image_bigger.jpg'
 };
 
-pablo = {
-	login: 'pablolis',
-	avatar: 'https://si0.twimg.com/profile_images/2225960371/PabloL_normal.jpg'
-};
-
-var carl = {
-	login: 'cletourneau',
-	avatar: 'https://si0.twimg.com/profile_images/2168665726/profile_pic_bigger.jpg'
-};
-
-var vince = {
-	login: 'testinfected',
-	avatar: 'https://si0.twimg.com/profile_images/268189950/vince_250x250_bigger.jpg'
-};
-
-andre = {
-	login: 'AndreBrissette',
-	avatar: 'https://si0.twimg.com/profile_images/1900139453/andre_brissette_200_bigger.jpg'
-};
-
-database.createPlayer(ericminio, function() {
-	database.createPlayer(pablo, function() {
-		database.createPlayer(carl, function() {
-			database.createPlayer(vince, function() {
-				database.createPlayer(andre, function() {
-				});
-			});
-		});
-	});
+database.createPlayer(ovaillancourt, function() {
 });
 
 server.start();
