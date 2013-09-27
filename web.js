4306e97b13666d3dac4d07475fe640d7b995638e
Eric Mignot <eric.mignot@gmail.com>
2013-09-26 21:51:14 -0400

    Welcome fperron :)

--- web.js
+++ web.js
@@ -10,8 +10,15 @@ var ericminio = {
 	login: 'ericminio',
 	avatar: 'https://si0.twimg.com/profile_images/848367249/moi_bigger.jpg'
 };
+var fperron = {
+	login: 'fperron',
+	avatar: 'https://si0.twimg.com/profile_images/2022210891/fperron-bw_bigger.png'
+};
 
 database.createPlayer(ericminio, function() {
+	database.createPlayer(fperron, function() {
+	});
 });
+
 
 server.start();
