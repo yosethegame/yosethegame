a76a158df3ad7d0e9985e79b3b1e622ba4fa8a8c
Eric Mignot <eric.mignot@gmail.com>
2013-09-27 15:47:15 -0400

    … Pretend that the home page displays as much lines as there are players in the database

--- public/js/home.page.js
+++ public/js/home.page.js
@@ -2,14 +2,19 @@ var fs 		  	= require('fs');
 var cheerio   	= require('cheerio');
 
 var extractPlayerTemplateIn = function(page) {
-	
+	return page.html('#players .player');
+};
+
+var buildPlayerList = function(page, database) {
+	return page.html();
 };
 
 index = function(request, response, database) {
 	var html = fs.readFileSync('./public/index.html').toString();
 
 	var page = cheerio.load(html);
-	var player_template = this.playerTemplate(page);
+	var list = this.buildPlayerList(page);
+	html = html.replace(this.extractPlayerTemplateIn(page), list);
 
 	response.write(html);
 	response.end();
@@ -17,3 +22,4 @@ index = function(request, response, database) {
 
 module.exports = index;
 module.exports.extractPlayerTemplateIn = extractPlayerTemplateIn;
+module.exports.buildPlayerList = buildPlayerList;
--- test/home.page.spec.js
+++ test/home.page.spec.js
@@ -1,5 +1,6 @@
-var home 	= require('../public/js/home.page');
-var cheerio = require('cheerio');
+var home 			 = require('../public/js/home.page');
+var cheerio 		 = require('cheerio');
+var InMemoryDatabase = require('./support/InMemoryDatabase');
 
 describe('Home page building', function() {
 
@@ -8,6 +9,18 @@ describe('Home page building', function() {
 		var page = cheerio.load(html);
 		
 		expect(home.extractPlayerTemplateIn(page)).toEqual('<li class="player"></li>');
+	});
+	
+	it('inserts two lines in player list when there are 2 players', function() {
+		var database = new InMemoryDatabase().withPlayers([
+				{ login: 'me' },
+				{ login: 'you' }
+			]);
+		var html = '<ul id="players"><li class="player"></li></ul>';
+		var page = cheerio.load(html);
+		var list = home.buildPlayerList(page, database);
+
+		expect(cheerio.load(list)('#players .player').length).toEqual(2);
 	});
 	
 });
