63d03f7582936bae96b38dcfe39d72df08e5b1b6
Eric Mignot <eric.mignot@gmail.com>
2013-09-30 08:35:11 -0400

    Pretend that the challenge.multiple.entries knows the expected answer for a string

--- public/challenge.multiple.entries/multiple.entries.response.matcher.js
+++ public/challenge.multiple.entries/multiple.entries.response.matcher.js
@@ -1,15 +1,23 @@
 var abstractMatcher = require('../js/abstract.response.matcher');
 var primeFactorsOf = require('../levels.common/prime.factors');
+var array = require('../js/utils/array.utils');
 
 module.exports = {
 
 	expectedContent: function(url) {
-		var numberIndex = url.indexOf('number=');
-		var number = url.substring(numberIndex + 'number='.length);
-		return {
-			number: parseInt(number),
-			decomposition: primeFactorsOf(number)
-		};
+		params = require('url').parse(url, true);
+		var expectedContent = [];
+		var numbers = params.query.number;
+		if (typeof numbers == 'string') {
+			numbers = [numbers];
+		}
+		array.forEach(numbers, function(number) {
+			expectedContent.push({
+				number: parseInt(number),
+				decomposition: primeFactorsOf(parseInt(number))
+			});
+		});
+		return expectedContent;
 	},
 		
 	validate: function(request, remoteResponse, content, callback) {		
--- test/challenge.multiple.entries/multiple.entries.response.matcher.spec.js
+++ test/challenge.multiple.entries/multiple.entries.response.matcher.spec.js
@@ -3,10 +3,13 @@ var matcher = require('../../public/challenge.multiple.entries/multiple.entries.
 describe('Multiple entries response matcher,', function() {
 
 	it('knows the expected answer for one number', function() {
-		expect(matcher.expectedContent('this-url/primeFactors?number=300')).toEqual({
-			number: 300,
-			decomposition: [2, 2, 3, 5, 5]
-		});
+		expect(matcher.expectedContent('this-url/primeFactors?number=300')).toEqual(
+			[
+				{
+					number: 300,
+					decomposition: [2, 2, 3, 5, 5]
+				}
+			]);
 	});
 	
 	it('knows the expected answer for two numbers', function() {
@@ -19,6 +22,20 @@ describe('Multiple entries response matcher,', function() {
 				{
 					number: 6,
 					decomposition: [2, 3]
+				},
+			]);
+	});
+	
+	it('knows the expected answer for a string', function() {
+		expect(matcher.expectedContent('this-url/primeFactors?number=15&number=hello')).toEqual(
+			[
+				{
+					number: 15,
+					decomposition: [3, 5]
+				},
+				{
+					number: "hello",
+					error: "not a number"
 				},
 			]);
 	});
