911a44e57ed0b449e71af5129eaaf4880afbae65
Eric Mignot <eric.mignot@gmail.com>
2013-10-13 18:40:56 -0400

    Pretend that the playground is not open to locked worlds

--- public/feature.playground/display.playground.request.js
+++ public/feature.playground/display.playground.request.js
@@ -6,8 +6,29 @@ var array		= require('../js/utils/array.utils');
 var withValue	= require('../js/utils/array.matchers');
 var thePlayer	= require('../js/utils/player.utils');
 
+var nextLevelOf = function(player, world) {
+	var levelIndex = 0;
+	if (!thePlayer.isANew(player)) {
+		while(array.hasOneItemIn(player.portfolio, withValue.equalsTo(world.levels[levelIndex].id))) { levelIndex ++; }
+	}
+	return levelIndex;
+};
+
+var fillBanner = function(page, player, world, worldNumber) {
+	page("#avatar").attr('src', player.avatar);
+	page('#score').text(renderScore(player.score));
+	var levelIndex = nextLevelOf(player, world);
+	var level = world.levels[levelIndex];
+	var greetings = 'level ' + worldNumber + '.' + (levelIndex + 1) + ' : ' + level.title;
+	page('#greetings').text(greetings);		
+	page('#dashboard-link').attr('href', '/players/' + player.login);
+};
+
 playground = function(request, response, database) {
 	var login = /^\/players\/(.*)\/play/.exec(request.url)[1];
+	var worldNumber = parseInt(/^\/players\/(.*)\/play\/world\/(.*)/.exec(request.url)[2]);
+	var world = database.worlds[worldNumber - 1];
+
 	var html = fs.readFileSync('./public/feature.playground/playground.html').toString();
 	var page = cheerio.load(html);
 	
@@ -19,20 +40,8 @@ playground = function(request, response, database) {
 			response.write(page.html());
 			response.end();
 			return;
-		}
-		
-		page("#avatar").attr('src', player.avatar);
-		page('#score').text(renderScore(player.score));
-		
-		var worldNumber = parseInt(/^\/players\/(.*)\/play\/world\/(.*)/.exec(request.url)[2]);
-		var world = database.worlds[worldNumber - 1];
-		var levelIndex = 0;
-		if (!thePlayer.isANew(player)) {
-			while(array.hasOneItemIn(player.portfolio, withValue.equalsTo(world.levels[levelIndex].id))) { levelIndex ++; }
-		}
-		var level = world.levels[levelIndex];
-		var greetings = 'level ' + worldNumber + '.' + (levelIndex + 1) + ' : ' + level.title;
-		page('#greetings').text(greetings);
+		}		
+		fillBanner(page, player, world, worldNumber);
 
 		response.write(page.html());
 		response.end();
---
+++ test/feature.playground/control.access.spec.js
@@ -0,0 +1,74 @@
+var cheerio 	= require('cheerio');
+var Data		= require('../support/database.with.levels');
+var playground	= require('../../public/feature.playground/display.playground.request.js');
+var response	= require('../support/fake.response');
+
+describe('Control Access', function() {
+	
+	var database = new Data();
+	var page;
+	var player;
+	
+	beforeEach(function() {	
+		database.worlds[0].isOpenFor = function(player) { return true; }
+		database.worlds[1].isOpenFor = function(player) { return true; }
+		player = {
+			login: 'ericminio'
+		}
+		database.players = [ player ];
+	});
+
+	describe('when player is known', function() {
+		
+		beforeEach(function() {
+			playground({ url: '/players/ericminio/play/world/1' }, response, database);
+			page = cheerio.load(response.html);
+		});
+		
+		it('unknown player mention is hidden', function() {
+			expect(page('#info').attr('class')).toContain('hidden');
+		});
+
+		it('player section is visible', function() {
+			expect(page('#player').attr('class')).toContain('visible');
+		});
+	});
+	
+	describe('when player is unknown', function() {
+		
+		beforeEach(function() {
+			playground({ url: '/players/any/play/world/1' }, response, database);
+			page = cheerio.load(response.html);
+		});
+		
+		it('unknown player mention is visible', function() {
+			expect(page('#info').attr('class')).toContain('visible');
+		});
+
+		it('player section is hidden', function() {
+			expect(page('#player').attr('class')).toContain('hidden');
+		});
+	});
+	
+	describe('when world is locked for the player', function() {
+		
+		beforeEach(function() {
+			database.worlds[1].isOpenFor = function(player) { return false; }
+			playground({ url: '/players/ericminio/play/world/2' }, response, database);
+			page = cheerio.load(response.html);
+		});
+		
+		it('info section is visible', function() {
+			expect(page('#info').attr('class')).toContain('visible');
+		});
+		
+		it('mentions that this world is locked', function() {
+			expect(page('#info').text()).toEqual('this world is locked');
+		});
+		
+		it('player section is hidden', function() {
+			expect(page('#player').attr('class')).toContain('hidden');
+		});
+
+	});
+});
