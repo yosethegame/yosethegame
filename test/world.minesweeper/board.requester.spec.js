944185fc8d22cb168453a6fb22c74b8b97930186
Eric Mignot <eric.mignot@gmail.com>
2013-10-27 11:53:10 -0400

    … Pretend that board.requester adds /minesweeper to the server url

---
+++ test/world.minesweeper/board.requester.spec.js
@@ -0,0 +1,15 @@
+var Requester = require('../../public/world.minesweeper/challenge.board/board.requester');
+
+describe('Board Requester', function() {
+
+	var requester;
+
+	beforeEach(function() {
+		requester = new Requester('this-url');
+	});
+	
+	it('adds /minesweeper to the url', function() {
+		expect(requester.url()).toEqual('this-url/minesweeper');
+	});
+		
+});
