d5dd13a20be61138d2b11df1d63f7fb3c0b4a3e6
Eric Mignot <eric.mignot@gmail.com>
2013-10-29 19:24:59 -0400

    The challenge exists :)

--- public/world.minesweeper/challenge.injection/injection.response.matcher.js
+++ public/world.minesweeper/challenge.injection/injection.response.matcher.js
@@ -2,25 +2,53 @@ var Browser = require('zombie');
 
 module.exports = {
 
+	data: [
+		['bomb' , 'empty', 'bomb' ],
+		['empty', 'empty', 'empty'],
+	],
+	
+	line: 1,
+	column: 3,
+
 	useData: function(data) {
 		this.data = data;
+	},
+	
+	playOnCell: function(line, column) {
+		this.line = line;
+		this.column = column;
 	},
 
 	validate: function(url, remoteResponse, content, callback) {
 		var self = this;
+		var cellId = 'cell-' + self.line +'x' + self.column;
 		var browser = new Browser();
 		browser.visit(url).
 			then(function() {
+				if(browser.evaluate("typeof load") != 'function') {
+					throw 'Error: missing load() method';
+				}
+				browser.document.grid = self.data;
+				var result = browser.evaluate('load()');
+				
+				var onclick = browser.query('[id=' + cellId + ']').onclick;
+				browser.evaluate(onclick);
+				
+				var classes = browser.query('[id=' + cellId + ']').className;
+				if (classes.indexOf('lost') == -1) {
+					throw "Error: #" + cellId + " class = '" + classes + "'"; 
+				}
+				
 				callback({
 					code: 200,
-					expected: 'green',
-					got: 'red'
+					expected: "A load() method and #" + cellId + " class containing 'lost'",
+					got: "A load() method and #" + cellId +" class containing 'lost'"
 				});
 			}).
 			fail(function(error) {
 				callback({
 					code: 501,
-					expected: 'green',
+					expected: "A load() method and #" + cellId + " class containing 'lost'",
 					got: error.toString()
 				});
 			});	
--- test/world.minesweeper/challenge.injection/injection.response.matcher.spec.js
+++ test/world.minesweeper/challenge.injection/injection.response.matcher.spec.js
@@ -5,6 +5,18 @@ describe('Minesweeper data injection', function() {
 	var status;
 	var remote;
 	
+	it('injects a grid with a bomb', function() {
+		expect(matcher.data).toEqual([
+				['bomb' , 'empty', 'bomb' ],
+				['empty', 'empty', 'empty'],
+			]);
+	});
+	
+	it('plays on cell 1,3', function() {
+		expect(matcher.line).toEqual(1);
+		expect(matcher.column).toEqual(3);
+	});
+	
 	describe('fails when load() function does not exist', function() {
 		
 		beforeEach(function() {
@@ -57,5 +69,111 @@ describe('Minesweeper data injection', function() {
 		});
 	});
 	
+	describe("fails when playing on a bomb does not change the class of the cell to 'lost'", function() {
+		
+		beforeEach(function() {
+			content = '<html><body>' +
+							'<label id="title">Minesweeper</label>' +
+
+							'<label id="cell-1x1" class="empty1" onclick="play(1, 1)"></label>' +
+							'<label id="cell-1x2" class="empty2" onclick="play(1, 2)"></label>' +
+							'<label id="cell-1x3" class="empty3" onclick="play(1, 3)"></label>' +
+							
+							'<script>function load() { }</script>' +
+							'<script>function play(line, column) { }</script>' +
+					  '</body></html>';			
+
+			remote = require('http').createServer(
+				function (request, response) {
+					response.write(content);
+					response.end();
+				})
+			.listen(6000);	
+			
+			matcher.useData([ [ 'bomb' , 'empty', 'bomb'] ]);		
+			matcher.playOnCell(1, 3);		
+		});
+		
+		afterEach(function() {
+			remote.close();
+		});
+
+		it('sets code to 501', function(done) {
+			matcher.validate('http://localhost:6000/minesweeper', {}, {}, function(status) {
+				expect(status.code).toEqual(501);
+				done();
+			});
+		});
+		
+		it('sets expected', function(done) {
+			matcher.validate('http://localhost:6000/minesweeper', {}, {}, function(status) {
+				expect(status.expected).toContain("#cell-1x3 class containing 'lost'");
+				done();
+			});
+		});
+		
+		it('sets actual', function(done) {
+			matcher.validate('http://localhost:6000/minesweeper', {}, {}, function(status) {
+				expect(status.got).toContain("#cell-1x3 class = 'empty3'");
+				done();
+			});
+		});
+	});
+	
+	describe("Passes when playing on a bomb sets class of the cell to 'lost'", function() {
+		
+		beforeEach(function() {
+			content = '<html><body>' +
+							'<label id="title">Minesweeper</label>' +
+
+							'<label id="cell-1x1" class="empty1" onclick="play(1, 1)"></label>' +
+							'<label id="cell-1x2" class="empty2" onclick="play(1, 2)"></label>' +
+							'<label id="cell-1x3" class="empty3" onclick="play(1, 3)"></label>' +
+							
+							'<script>function load() { }</script>' +
+							'<script>function play(line, column) { ' +
+							' 			 var id = "cell-" + line + "x" + column;' +
+							' 			 var state = document.grid[line-1][column-1] == "bomb" ? "lost" : "";' +
+							'            document.getElementById(id).className = state; ' +
+							'        }' +
+							'</script>' +
+					  '</body></html>';			
+
+			remote = require('http').createServer(
+				function (request, response) {
+					response.write(content);
+					response.end();
+				})
+			.listen(6000);	
+			
+			matcher.useData([ [ 'bomb' , 'empty', 'bomb'] ]);
+			matcher.playOnCell(1, 3);		
+		});
+		
+		afterEach(function() {
+			remote.close();
+		});
+
+		it('sets code to 200', function(done) {
+			matcher.validate('http://localhost:6000/minesweeper', {}, {}, function(status) {
+				expect(status.code).toEqual(200);
+				done();
+			});
+		});
+		
+		it('sets expected', function(done) {
+			matcher.validate('http://localhost:6000/minesweeper', {}, {}, function(status) {
+				expect(status.expected).toContain("A load() method and #cell-1x3 class containing 'lost'");
+				done();
+			});
+		});
+		
+		it('sets actual', function(done) {
+			matcher.validate('http://localhost:6000/minesweeper', {}, {}, function(status) {
+				expect(status.got).toContain("A load() method and #cell-1x3 class containing 'lost'");
+				done();
+			});
+		});
+	});
 	
 });
