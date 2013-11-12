0deaa097b14fab9585b0b75bea31d722a7340cb6
Eric Mignot <eric.mignot@gmail.com>
2013-11-12 17:58:27 -0500

    ExtractMethod to reduce LongMethod

--- public/js/serve-content.js
+++ public/js/serve-content.js
@@ -1,24 +1,31 @@
-var fs = require('fs');
-var array = require('./utils/array.utils');
+var fs      = require('fs');
+var array   = require('./utils/array.utils');
+
+var contentTypes = [
+    { extension: 'css', contentType: 'text/css'},
+    { extension: 'js', contentType: 'application/javascript'},
+    { extension: 'jpeg', contentType: 'image/jpeg'},
+    { extension: 'png', contentType: 'image/png'},
+    { extension: '', contentType: 'text/plain'}
+];
+
+var matching = function(extension) {
+    return function(item) { 
+        return extension.indexOf(item.extension) != -1; 
+    };
+};
+
+var extensionOf = function(fileName) {
+    var segments = fileName.split('.');
+    return segments[segments.length - 1];
+};
 
 servecontent = function(folder) {
 	
 	return function (request, response) {
 		var candidate = folder + (request.url == '/' ? '/index.html' : request.url);
 		if (fs.existsSync(candidate) && (fs.statSync(candidate).isFile())) {
-            var segments = candidate.split('.');
-            var extension = segments[segments.length - 1];
-            
-            var contentTypes = [
-                { extension: 'css', contentType: 'text/css'},
-                { extension: 'js', contentType: 'application/javascript'},
-                { extension: 'jpeg', contentType: 'image/jpeg'},
-                { extension: 'png', contentType: 'image/png'},
-                { extension: '', contentType: 'text/plain'}
-            ];
-            var contentType = array.firstItemIn(contentTypes, function(item) { 
-                return extension.indexOf(item.extension) != -1; 
-            } ).contentType;
+            var contentType = array.firstItemIn(contentTypes, matching(extensionOf(candidate))).contentType;
             
             response.writeHead(200, { 'Content-Type' : contentType });
 			response.write(fs.readFileSync(candidate));			
