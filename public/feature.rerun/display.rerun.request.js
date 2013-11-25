dbc6577a7a1be4940ef995ebb4a39bb05075e3a5
Eric Mignot <eric.mignot@gmail.com>
2013-11-25 17:34:18 -0500

    A player can rerun the completed levels of a world

--- public/feature.rerun/display.rerun.request.js
+++ public/feature.rerun/display.rerun.request.js
@@ -5,12 +5,23 @@ var renderScore = require('../js/utils/render.score');
 var array       = require('../js/utils/array.utils');
 var withValue   = require('../js/utils/array.matchers');
 var thePlayer   = require('../js/utils/player.utils');
+var tryRequest  = require('../feature.try/try.request');
+var thisPlayer  = require('../js/utils/player.utils');
+var request     = require('request');
 
 var fillBanner = function(page, player, world, worldNumber) {
 	page("#avatar").attr('src', player.avatar);
 	page('#score').text(renderScore(player.score));
 	page('#dashboard-link').attr('href', '/players/' + player.login);
 	page('#greetings').text('Rerun the completed levels of ' + world.name);
+};
+
+var showServerOfPlayer = function(page, player) {
+	if (thePlayer.hasServer(player)) {
+		var serverOfPlayer = thePlayer.serverOf(player);
+		page('#server-of-player').addClass('visible').removeClass('hidden').empty().append(serverOfPlayer);
+		page('#server-of-player').attr('href', serverOfPlayer);
+	}	
 };
 
 var exitWithMessage = function(message, page, response) {
@@ -22,7 +33,42 @@ var exitWithMessage = function(message, page, response) {
 	return;	
 };
 
-rerun = function(request, response, database) {
+var responseCount;
+var output;
+var levelsToTry;
+
+var tryLevelAtIndex = function(index, player, database, response, callback) {
+	var level = levelsToTry[index];
+	var Requester = require(level.requester);
+	var requester = new Requester(thisPlayer.serverOf(player));
+	var requestSent = requester.url();
+	if (requestSent === undefined) requestSent = '';
+
+	request(requestSent, function(error, remoteResponse, content) {
+		var checker = require(level.checker);
+		checker.validate(requestSent, remoteResponse, content, function(status) {
+			if (error !== null) {
+				status.code = 404;
+				status.got = 'undefined';
+			}
+			output.push({
+				id: level.id,
+				title: level.title,
+				code: status.code,
+				expected: status.expected,
+				got: status.got
+			});
+			if (index < levelsToTry.length-1) {
+				tryLevelAtIndex(index + 1, player, database, response, callback);
+			}
+			else {
+				callback();
+			}
+		});
+	});
+};
+
+rerun = function(request, response, database, done) {
 	var html = fs.readFileSync('./public/feature.rerun/rerun.html').toString();
 	var page = cheerio.load(html);
 	
@@ -46,9 +92,34 @@ rerun = function(request, response, database) {
 		page('#login').empty().text(player.login);
 			
 		fillBanner(page, player, world, worldNumber);
+		showServerOfPlayer(page, player);
 		
-		response.write(page.html());
-		response.end();
+    	output = [];
+		levelsToTry = tryRequest.allLevelsToTry(player, world);
+		responseCount = levelsToTry.length;
+		tryLevelAtIndex(0, player, database, response, function() {
+            var result_n_html = page.html('#result_1');
+            page('#result_1').remove();
+            for (var i=0; i<output.length; i++) {
+                var result_i_html = result_n_html.replace('result_1', 'result_' + (i+1));
+        		page('#results').append(result_i_html);
+        		page('#result_' + (i+1) + ' .challenge').text(output[i].title);
+        		page('#result_' + (i+1) + ' .status').text(output[i].code == 200 ? 'success': 'fail');
+        		page('#result_' + (i+1) + ' .expected').text(typeof output[i].expected == 'string' ? output[i].expected: JSON.stringify(output[i].expected));
+        		page('#result_' + (i+1) + ' .got').text(typeof output[i].got == 'string' ? output[i].got: JSON.stringify(output[i].got));
+        		
+        		if (output[i].code != 200) {
+        		    page('#result_' + (i+1)).removeClass('success').addClass('danger');
+        		}
+            };
+
+    		response.write(page.html());
+    		response.end();
+    		if (done != undefined) {
+    		    done();
+		    }
+		});
+		
 	});
 	
 };
