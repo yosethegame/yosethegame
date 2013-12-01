50144be35ac3db98e1c6ac7f11badfa68ef5d744
Eric Mignot <eric.mignot@gmail.com>
2013-11-30 23:43:03 -0500

    a textual feedback is displayed next to the avatar preview at player creation

--- public/feature.create.player/create.player.listener.js
+++ public/feature.create.player/create.player.listener.js
@@ -1,11 +1,43 @@
 var $ = $ || require('jquery');
 
-function CreatePlayerListener() {	
+function CreatePlayerListener() {
+    self = this;	
 }
+
+CreatePlayerListener.prototype.updatePreview = function() {
+    $('#avatar-preview').attr('src', $('#avatar').val());
+    $.get($('#avatar').val()).success(this.succesGettingAvatar).error(this.errorGettingAvatar);
+};
 
 CreatePlayerListener.prototype.player = function() {
     var login = $('#login').val().replace(/\s/g, '');
-	$.post('/create-player', { login: login, avatar: $('#avatar').val() }, this.success);
+    var avatar = $('#avatar').val();
+
+	$.post('/create-player', { login: login, avatar: avatar }, this.success);
+};
+
+CreatePlayerListener.prototype.succesGettingAvatar = function(data, textStatus, jqXHR) {
+    var headers = jqXHR.getAllResponseHeaders();
+    var type = headers.indexOf('Content-Type: image');
+    if (type === -1) {
+        self.displayError();
+    } else {
+        self.displaySuccess();
+    }
+};
+
+CreatePlayerListener.prototype.errorGettingAvatar = function(jqXHR, textStatus, errorThrown) {
+    self.displayError();
+};
+
+CreatePlayerListener.prototype.displayError = function() {
+    $('#preview-feedback').removeClass('alert-success').addClass('alert-danger');
+    $('#preview-feedback label').text('Not an image');
+};
+
+CreatePlayerListener.prototype.displaySuccess = function() {
+    $('#preview-feedback').removeClass('alert-danger').addClass('alert-success');
+    $('#preview-feedback label').text('Image found');
 };
 
 CreatePlayerListener.prototype.success = function(data) {
--- spec/create.player.preview.avatar.spec.js
+++ spec/create.player.preview.avatar.spec.js
@@ -3,7 +3,7 @@ var router = require('../public/js/router');
 var Server = require('../public/js/server');
 var InMemoryDatabase = require('../test/support/database.with.levels');
 
-describe('Creating a player', function() {
+describe('Avatar preview', function() {
 
 	var server = new Server(router);
 	
@@ -18,7 +18,7 @@ describe('Creating a player', function() {
 		server.stop();
 	});
 	
-	it('makes the dashboard of this player available', function(done) {
+	it('is updated with the chosen url', function(done) {
 		var browser = new Browser();
 		browser.visit('http://localhost:5000/create-new-player').
 			then(function () {
@@ -27,6 +27,46 @@ describe('Creating a player', function() {
 			}).
 			then(function() {
 				expect(browser.query('#avatar-preview').src).toEqual('http://this-url/');
+				done();
+			}).
+			fail(function(error) {
+				expect(error.toString()).toBeNull();
+				done();
+			});
+	});
+	
+	it('is confirmed correct by a message', function(done) {
+		var browser = new Browser();
+		browser.visit('http://localhost:5000/create-new-player').
+			then(function () {
+				return browser.fill('#login', 'eric')
+							  .fill('#avatar', '/img/default-avatar.png');
+			}).
+			then(function() {
+				expect(browser.query('#preview-feedback').className).toContain('alert-success');
+			}).
+			then(function() {
+				expect(browser.text('#preview-feedback label')).toContain('Image found');
+				done();
+			}).
+			fail(function(error) {
+				expect(error.toString()).toBeNull();
+				done();
+			});
+	});
+
+	it('is confirmed incorrect by a message when incorrect', function(done) {
+		var browser = new Browser();
+		browser.visit('http://localhost:5000/create-new-player').
+			then(function () {
+				return browser.fill('#login', 'eric')
+							  .fill('#avatar', 'http://this-url');
+			}).
+			then(function() {
+				expect(browser.query('#preview-feedback').className).toContain('alert-danger');
+			}).
+			then(function() {
+				expect(browser.text('#preview-feedback label')).toContain('Not an image');
 				done();
 			}).
 			fail(function(error) {
