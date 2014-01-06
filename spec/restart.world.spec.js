de04a2d9313fd77d991b2f6b6dbde5dac73cbea2
Eric Mignot <eric.mignot@gmail.com>
2014-01-06 07:56:23 -0500

    Pretend that a player can restart a world

---
+++ spec/restart.world.spec.js
@@ -0,0 +1,55 @@
+var DatabaseWithChallenges 	= require('../test/support/database.with.levels');
+var Browser 				= require("zombie");
+var router 					= require('../public/js/router');
+var Server 					= require('../public/js/server');
+
+describe("When a player restarts world #2", function() {
+
+	var server = new Server(router);
+	var database;
+	
+	beforeEach(function(done) {
+		database = new DatabaseWithChallenges();
+		database.players = [
+			{
+				login: 'bilou',
+				portfolio: [ { server: 'http://localhost:6000', achievements: [1, 3] } ]
+			}
+		];
+		server.useDatabase(database);
+		server.start();
+
+		var browser = new Browser();
+		browser.visit('http://localhost:5000/players/bilou').
+			then(function () {
+				return browser.clickLink("#restart-world-2-link");
+			}).
+			then(function() {
+				done();
+			}).
+			fail(function(error) {
+				expect(error.toString()).toBeNull();
+				done();
+			});
+	});
+
+	afterEach(function() {
+		server.stop();
+	});
+
+	it('Then the first challenge of world #2 is displayed as an invitation', function(done) {
+		var browser = new Browser();
+		browser.visit('http://localhost:5000/players/bilou').
+			then(function() {
+				expect(browser.text("#worlds tr:nth-child(2) td:nth-child(2) ul.levels li:nth-child(1) a")).toContain(database.worlds[1].levels[0].title);
+				done();
+			}).
+			fail(function(error) {
+				expect(error.toString()).toBeNull();
+				done();
+			});
+	});
+	
+});
+		
+		
