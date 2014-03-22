326e7177e09930b3f54ba86650d8f8f7620596a7
Eric Mignot <eric.mignot@gmail.com>
2014-03-22 10:11:50 -0400

    Thx to @_ewingd who pointed out a missing test

--- app/challenges/world.canadair/challenge.first.fire/lib/what.is.below.plane.js
+++ app/challenges/world.canadair/challenge.first.fire/lib/what.is.below.plane.js
@@ -1,4 +1,8 @@
 whatIsBelowPlaneIn = function(map, plane) {
+    
+    if (plane.y < 0) { return undefined; }
+    if (plane.y > (map.length-1)) { return undefined; }
+    
     return map[plane.y][plane.x];
 };
 
--- app/challenges/world.canadair/challenge.first.fire/what.is.below.the.plane.spec.js
+++ app/challenges/world.canadair/challenge.first.fire/what.is.below.the.plane.spec.js
@@ -24,5 +24,24 @@ describe('whatIsBelowPlaneIn method', function() {
     it('returns F when the plane is above the fire', function() {
         expect(whatIsBelowPlaneIn(map, new Plane(1, 2))).toEqual('F');
     });
+    
+    describe('when the plane is outside the map', function() {
+        
+        it('returns undefined when too far right', function() {
+            expect(whatIsBelowPlaneIn(map, new Plane(map[0].length, 0))).toEqual( undefined );
+        });
 
+        it('returns undefined when too far bottom', function() {
+            expect(whatIsBelowPlaneIn(map, new Plane(0, map.length))).toEqual( undefined );
+        });
+
+        it('returns undefined when too far left', function() {
+            expect(whatIsBelowPlaneIn(map, new Plane(-1, 0))).toEqual( undefined );
+        });
+
+        it('returns undefined when too far up', function() {
+            expect(whatIsBelowPlaneIn(map, new Plane(0, -1))).toEqual( undefined );
+        });
+
+    });
 });
