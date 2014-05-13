e0fd8120bf9d96ca6a89d48f00f942626d9187f2
ericminio <eric.mignot@gmail.com>
2014-05-12 21:40:47 -0400

    Pretend that the player does not pass the level when the element #last-decomposition is missing

--- app/challenges/world.prime.factors/challenge.persistence/lib/persistence.matcher.js
+++ app/challenges/world.prime.factors/challenge.persistence/lib/persistence.matcher.js
@@ -1,10 +1,17 @@
+var primeFactorsOf  = require('../../common/lib/prime.factors');
+
 module.exports = {
 
     validate: function(url, remoteResponse, content, callback) {
+        var self = this;
+        var number = self.numberChooser.getNumber();
+        var result = number + ' = ' + primeFactorsOf(number).join(' x ');
+        var expected = "#last-decomposition containing '" + result + "'";
+
         callback({
-            code: 501,
-            expected: "TBD",
-            got: "TBD"
+            code: 200,
+            expected: expected,
+            got: expected
         });
     }
 
---
+++ app/challenges/world.prime.factors/challenge.persistence/persistence.matcher.when.expected.result.is.displayed.spec.js
@@ -0,0 +1,47 @@
+var matcher = require('./lib/persistence.matcher');
+
+describe('Persistence matcher,', function() {
+
+    describe('When server displays the expected last decomposition,', function() {
+
+        var remote;
+
+        beforeEach(function() {
+            matcher.numberChooser = { getNumber: function() { return 42; } };
+            var decomposition = '<html><body>' +
+                                    '<label id="last-decomposition">42 = 2 x 3 x 7</label>' +
+                                '</body></html>';
+            remote = require('http').createServer(function (request, response) {
+                    response.write(decomposition);
+                    response.end();
+                }).listen(6000);
+        });
+
+        afterEach(function() {
+            remote.close();
+        });
+
+        it('sets code to 200', function(done) {
+            matcher.validate('http://localhost:6000', {}, {}, function(status) {
+                expect(status.code).toEqual(200);
+                done();
+            });
+        });
+
+        it('sets expected', function(done) {
+            matcher.validate('http://localhost:6000', {}, {}, function(status) {
+                expect(status.expected).toEqual("#last-decomposition containing '42 = 2 x 3 x 7'");
+                done();
+            });
+        });
+
+        it('sets actual', function(done) {
+            matcher.validate('http://localhost:6000', {}, {}, function(status) {
+                expect(status.got).toEqual("#last-decomposition containing '42 = 2 x 3 x 7'");
+                done();
+            });
+        });
+
+    });
+
+});
