c04c46889e3ce2df6dadd052cb8ee668e8114941
ericminio <eric.mignot@gmail.com>
2014-07-02 06:17:32 -0400

    ... Rename tests to be more specific

--- app/challenges/world.fire/challenge.emergency/4.must.extinguish.the.fire.spec.js
+++ app/challenges/world.fire/challenge.emergency/4.must.extinguish.the.fire.spec.js
@@ -1,10 +1,10 @@
-var matcher = require('./lib/first.fire.response.matcher');
+var matcher = require('./lib/emergency.response.matcher');
 
 var failWhenThePlane = require('../challenge.common/fail.when.the.plane');
 
 describe('First fire response matcher,', function() {
     
-	it('makes flight-related verifications', function() {
+	it('checks that the plane extinguishes the fire', function() {
 		failWhenThePlane.neverTakesWater(matcher);
 		failWhenThePlane.neverReachesTheFire(matcher);
 		failWhenThePlane.reachesTheFireBeforeTakingWater(matcher);
