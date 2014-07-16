c6ec765e64c31b36c7d6901b825337dc3155a9c2
ericminio <eric.mignot@gmail.com>
2014-07-16 09:21:08 -0400

    Rename test helpers to reduce BadName

--- app/challenges/challenge.ping/ping.challenge.spec.js
+++ app/challenges/challenge.ping/ping.challenge.spec.js
@@ -1,23 +1,23 @@
 var matcher = require('./lib/ping.response.matcher');
-var failWhenTheHeader = require('../common/fail.when.the.header');
-var failWhenTheRemoteServer = require('../common/fail.when.the.remote.server');
-var failWhenTheReceivedContent = require('../common/fail.when.the.received.content');
-var passWhenTheReceivedContent = require('../common/pass.when.the.received.content');
+var failsWhenTheHeader = require('../common/fails.when.the.header');
+var failsWhenTheRemoteServer = require('../common/fails.when.the.remote.server');
+var failsWhenTheReceivedContent = require('../common/fails.when.the.received.content');
+var passesWhenTheReceivedContent = require('../common/passes.when.the.received.content');
 
 describe('Ping challenge,', function() {
 	
 	it('expects a running remote server', function() {
-		failWhenTheRemoteServer.doesNotAnswer(matcher);
+		failsWhenTheRemoteServer.doesNotAnswer(matcher);
 	});
 	
 	it('is a json challenge', function() {
-		failWhenTheHeader.isEmpty(matcher);
-		failWhenTheHeader.isNotApplicationJson(matcher);
+		failsWhenTheHeader.isEmpty(matcher);
+		failsWhenTheHeader.isNotApplicationJson(matcher);
 	});
 	
 	it('expects a specific static response', function() {
-		failWhenTheReceivedContent.isNot({alive:true}, matcher);
-		passWhenTheReceivedContent.is({alive:true}, matcher);
+		failsWhenTheReceivedContent.isNot({alive:true}, matcher);
+		passesWhenTheReceivedContent.is({alive:true}, matcher);
 	});
 	
 });
