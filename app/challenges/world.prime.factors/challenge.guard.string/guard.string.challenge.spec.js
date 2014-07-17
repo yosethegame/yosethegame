036e831425213d958ab3da481769d48c5102cfa0
ericminio <eric.mignot@gmail.com>
2014-07-17 14:51:35 -0400

    Document Guard string challenge

--- app/challenges/world.prime.factors/challenge.guard.string/guard.string.challenge.spec.js
+++ app/challenges/world.prime.factors/challenge.guard.string/guard.string.challenge.spec.js
@@ -1,6 +1,7 @@
 var matcher = require('./lib/guard.response.matcher');
 var failsWhenTheRemoteServer = require('../../common/fails.when.the.remote.server');
 var failsAnswering = require('../../common/fails.answering.request');
+var passesAnswering = require('../../common/passes.answering.request');
 
 describe('Guard string challenge,', function() {
 	
@@ -8,9 +9,19 @@ describe('Guard string challenge,', function() {
 		failsWhenTheRemoteServer.doesNotAnswer(matcher);
 	});
 	
-	it('is a dynamic json challenge', function() {
+	it('expects a json header', function() {
 		failsAnswering('***?number=batman').whenTheHeaderIsEmpty(matcher);
-		failsAnswering('***?number=batman').whenTheHeaderIsNotApplicationJson(matcher);
-		failsAnswering('***?number=batman').whenTheResponseIsNotInJsonFormat(matcher);
+		failsAnswering('***?number=batman').whenTheHeaderIsNotApplicationJson(matcher);		
+	});
+	
+	it('expect a specific json response', function() {
+		passesAnswering('***?number=batman').whenTheAnswerIs({ number : 'batman', error : 'not a number' }, matcher);
+		passesAnswering('***?number=batman').whenTheAnswerIs({ error : 'not a number', number : 'batman' }, matcher);
+
+		failsAnswering('***?number=batman', { number : 'batman', error : 'not a number' }).whenTheAnswerIs({ value: 42 }, matcher);
+		failsAnswering('***?number=batman', { number : 'batman', error : 'not a number' }).whenTheAnswerIs('anything but json', matcher);
+		failsAnswering('***?number=batman', { number : 'batman', error : 'not a number' }).whenTheAnswerIs({ number: 'batman' }, matcher);
+		failsAnswering('***?number=batman', { number : 'batman', error : 'not a number' }).whenTheAnswerIs({ error : 'not a number' }, matcher);
+		failsAnswering('***?number=batman', { number : 'batman', error : 'not a number' }).whenTheAnswerIs({ number : 'other', error : 'not a number' }, matcher);
 	});
 });
