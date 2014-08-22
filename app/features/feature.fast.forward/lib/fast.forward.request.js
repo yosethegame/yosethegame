5136326d90a71e07a281a04593d184381349fd29
ericminio <eric.mignot@gmail.com>
2014-08-22 15:08:06 -0400

    ... scores up correctly

--- app/features/feature.fast.forward/lib/fast.forward.request.js
+++ app/features/feature.fast.forward/lib/fast.forward.request.js
@@ -1,6 +1,32 @@
+var tryRequest = require('../../feature.try/lib/try.request');
+var logSuccess = require('../../feature.try/lib/log.success');
+var array = require('../../../utils/lib/array.utils');
+var thisPlayer = require('../../../lib/player.utils');
+
 var fastforward = function(request, response, database, done) {
-    response.end();
-    done();
+    
+    var login = /^\/players\/(.*)\/fastforward/.exec(request.url)[1];
+    var worldNumber = parseInt(/^\/players\/(.*)\/fastforward\/world\/(.*)/.exec(request.url)[2]);
+    var world = database.worlds[worldNumber - 1];
+    
+    database.find(login, function(player) {
+
+        tryRequest.tryLevelsStartingAtIndex(0, world.levels, {}, player, database, [], function(output) {
+            var stopHere = false;
+            array.forEach(output, function(result) {
+                if (!stopHere && result.code === 200 && ! thisPlayer.hasDoneThisLevel(player, result)) {
+                    logSuccess(player, result.id);
+                }
+                if (result.code !== 200) { stopHere = true; }
+            });
+            response.end();
+
+            if (done !== undefined) {
+                done();
+            }
+        });        
+    });
+    
 };
 
 module.exports = fastforward;
--- app/features/feature.fast.forward/scores.up.spec.js
+++ app/features/feature.fast.forward/scores.up.spec.js
@@ -5,37 +5,58 @@ var response    = require('../../support/fake.response');
 
 describe('Fast-forwarding', function() {
     
+    var remote;
+    var port = 6000;
+    var request = { url: '/players/ericminio/fastforward/world/1' };
     var database = new Data();
     var player;
-    var remote;
     
     beforeEach(function(done) { 
-        remote = require('http').createServer(
-            function (request, response) {
-                response.writeHead(200);
-                response.end();
-            })
-        .listen(6000, done);
+        player = {
+            login: 'ericminio',
+            score: 0,
+            portfolio: [ { server: 'http://localhost:' + port, achievements: [] } ]
+        };
+        database.players = [ player ];
+        database.worlds[0].isOpenFor = function(player) { return true; };
+        database.worlds[0].levels[0].checker = '../../../support/response.always.valid';
+        database.worlds[0].levels[1].checker = '../../../support/response.always.valid';
+
+        remote = require('http').createServer(function (request, response) { response.end(); }).listen(port, done);
     });
     
     afterEach(function() {
         remote.close();
     });
     
-    it('scores up the passed level', function(done) {
-        database.worlds[0].isOpenFor = function(player) { return true; };
-        database.worlds[0].levels[0].checker = '../../../support/response.always.valid';
-        database.worlds[0].levels[1].checker = '../../../support/response.always.valid';
-        player = {
-            login: 'ericminio',
-            score: 0,
-            portfolio: [ { server: 'http://localhost:6000', achievements: [] } ]
-        };
-        database.players = [ player ];
-        fastforward({ url: '/players/ericminio/fastforward/world/1' }, response, database, function() {
+    it('scores up the passed levels', function(done) {
+        fastforward(request, response, database, function() {
             expect(player.score).toEqual(20);
             done();
         });
     });
     
+    it('does not score up the failing levels', function(done) {
+        database.worlds[0].levels[1].checker = '../../../support/response.always.501';
+        fastforward(request, response, database, function() {
+            expect(player.score).toEqual(10);
+            done();
+        });
+    });
+    
+    it('does not score up an already passed level', function(done) {
+        player.portfolio[0].achievements = [1];
+        fastforward(request, response, database, function() {
+            expect(player.score).toEqual(10);
+            done();
+        });
+    });
+    
+    it('stops after the first failing level', function(done) {
+        database.worlds[0].levels[0].checker = '../../../support/response.always.501';
+        fastforward(request, response, database, function() {
+            expect(player.score).toEqual(0);
+            done();
+        });
+    });
 });
