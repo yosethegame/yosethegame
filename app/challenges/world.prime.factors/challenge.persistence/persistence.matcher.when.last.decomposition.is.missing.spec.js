d28445df905df5c77e3ab48b4e4ce651a0939009
ericminio <eric.mignot@gmail.com>
2014-12-10 23:12:15 -0500

    Remove useless remote server in test

--- app/challenges/world.prime.factors/challenge.persistence/persistence.matcher.when.last.decomposition.is.missing.spec.js
+++ app/challenges/world.prime.factors/challenge.persistence/persistence.matcher.when.last.decomposition.is.missing.spec.js
@@ -4,43 +4,32 @@ describe('Persistence matcher,', function() {
 
     describe('When element last decomposition is missing,', function() {
 
-        var remote;
-        var decomposition;
+        var content;
 
         beforeEach(function() {
-            matcher.numberChooser = { getNumber: function() { return 42; } };
-            decomposition = '<html><body></body></html>';
-            remote = require('http').createServer(function (request, response) {
-                response.write(decomposition);
-                response.end();
-            }).listen(6000);
-        });
-
-        afterEach(function() {
-            remote.close();
+            content = '<html><body></body></html>';
         });
 
         it('sets code to 501', function(done) {
-            matcher.validate('http://localhost:6000', {}, decomposition, function(status) {
+            matcher.validate('http://localhost:6000', {}, content, function(status) {
                 expect(status.code).toEqual(501);
                 done();
             });
         });
 
         it('sets expected', function(done) {
-            matcher.validate('http://localhost:6000', {}, decomposition, function(status) {
+            matcher.validate('http://localhost:6000', {}, content, function(status) {
                 expect(status.expected).toEqual("#last-decomposition containing '42 = 2 x 3 x 7'");
                 done();
             });
         });
 
         it('sets actual', function(done) {
-            matcher.validate('http://localhost:6000', {}, decomposition, function(status) {
+            matcher.validate('http://localhost:6000', {}, content, function(status) {
                 expect(status.got).toEqual("#last-decomposition is missing");
                 done();
             });
         });
 
     });
-
 });
